44e591cd1d17a3a9210dcb993b2cee08b36d3837
Gustavo P <guprado.silva@gmail.com>
2023-08-26 16:47:29 -0300

    [add] Minimap on bottom right, better layer rendering + support for more layers

---
+++ README.md
@@ -0,0 +1,13 @@
+# Old School Dungeon Crawler Renderer
+
+# Project ⚔️
+
+Old-school dungeon crawler type renderer, based on games like Phantasy Start, Shin Megami Tensei, etc
+
+# Roadmap 🗺️
+
+- [ ] Smoother Animation
+- [ ] Doors, Stairs and other props
+- [ ] Enemies!
+- [ ] Bigger Maps!
+- [ ] > > MAPPING TOOL <<
--- script.js
+++ script.js
@@ -7,36 +7,28 @@ if (canvas) {
    const GRID_SIZE   = 8;
    const LINE_SIZE   = SCREEN_SIZE/GRID_SIZE;  
 
-   let player_lin = 5;
-   let player_col = 4;
+   let player_lin = 6;
+   let player_col = 6;
    let dir = 1;
 
    let map = [
       [1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 1, 1, 1, 1, 0, 1],
       [1, 0, 0, 0, 0, 0, 0, 1],
-      [1, 0, 0, 1, 1, 1, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 1, 1, 0, 1, 0, 1],
+      [1, 0, 1, 1, 0, 1, 0, 1],
       [1, 0, 0, 1, 0, 1, 0, 1],
-      [1, 0, 0, 1, 0, 1, 0, 1],
-      [1, 0, 0, 1, 1, 1, 0, 1],
       [1, 1, 1, 1, 1, 1, 1, 1]
    ]
 
    // Sprites da primeira layer
-   let img_Center1 = new Image();
-   let img_Left1 = new Image();
-   let img_Right1 = new Image();
-   img_Center1.src = './Center1.png';
-   img_Left1.src = './Left1.png';
-   img_Right1.src = './Right1.png';
-
-   // Sprites da segunda layer
-   let img_Center2 = new Image();
-   let img_Left2 = new Image();
-   let img_Right2 = new Image();
-   img_Center2.src = './Center2.png';
-   img_Left2.src = './Left2.png';
-   img_Right2.src = './Right2.png';
+   let spr_wall_center = new Image();
+   let spr_wall_left   = new Image();
+   let spr_wall_right  = new Image();
+   spr_wall_center.src = './assets/sprites/wall_center.png';
+   spr_wall_left.src   = './assets/sprites/wall_left.png';
+   spr_wall_right.src  = './assets/sprites/wall_right.png';
 
 
    function rotateLeft() {
@@ -53,148 +45,86 @@ if (canvas) {
       player_lin = 7-aux;
    }
 
-   function drawFrontWalls(sizeX, sizeY) {
-      // Desenha parte de cima parede
-      ctx.strokeRect((SCREEN_SIZE-sizeX)/2, (SCREEN_SIZE-sizeY)/2, sizeX, sizeY);
-      ctx.stroke();
-   }
-
-   function drawSideWalls(startX, startY, endX, endY) {
-      // Desenha parte de cima parede
-      ctx.beginPath();
-      ctx.moveTo(startX, startY);
-      ctx.lineTo(endX, endY);
-      ctx.stroke();
-
-      // Desenha parte de baixo parede
-      ctx.beginPath();
-      ctx.moveTo(startX, SCREEN_SIZE-startY);
-      ctx.lineTo(endX, SCREEN_SIZE-endY);
-      ctx.stroke();
-
-      // Desenha divisao da parede
-      ctx.beginPath();
-      ctx.moveTo(endX, endY);
-      ctx.lineTo(endX, SCREEN_SIZE-endY);
-      ctx.stroke();
-   }
-
-   function drawFirstLayer(player_lin, player_col, dir) {
-      let done = false;
-
-      // Checa primeira layer por paredes
-      if(map[player_lin-dir][player_col] == 1) {
-         ctx.drawImage(img_Center1, SCREEN_SIZE/4, SCREEN_SIZE/4, 256, 256);
-         done = true;
+   function darkenLayer(posX, posY, sizeX, sizeY, qtd) {
+      const imageData = ctx.getImageData(posX, posY, sizeX, sizeY);
+      const data = imageData.data;
+      for (let i = 0; i < data.length; i += 4) {
+         data[i] -= qtd*20; // red
+         data[i + 1] -= qtd*20; // green
+         data[i + 2] -= qtd*20; // blue
       }
-      if(map[player_lin][player_col-dir] == 0) {
-         ctx.drawImage(img_Center1, SCREEN_SIZE/4-256, SCREEN_SIZE/4, 256, 256);
-      }
-      if(map[player_lin][player_col+dir] == 0) {
-         ctx.drawImage(img_Center1, SCREEN_SIZE/4+256, SCREEN_SIZE/4, 256, 256);
-      }
-
-      // Parede esquerda
-      if(map[player_lin][player_col-dir] == 1) {
-         ctx.drawImage(img_Left1, 0, 0, 128, 512);
-      }
-      // Parede direita
-      if(map[player_lin][player_col+dir] == 1) {
-         ctx.drawImage(img_Right1, 3*SCREEN_SIZE/4, 0, 128, 512);
-      }
-
-      return done;
-   }
-
-   function drawSecondLayer(player_lin, player_col, dir) {
-      let done = false;
-
-      // Checa segunda layer por paredes
-      if(map[player_lin-(2*dir)][player_col] == 1) {
-         ctx.drawImage(img_Center2, 3*SCREEN_SIZE/8, 3*SCREEN_SIZE/8, 128, 128);
-         done = true;
-      }
-      if(map[player_lin-dir][player_col-dir] == 0) {
-         ctx.drawImage(img_Center2, 3*SCREEN_SIZE/8-64, 3*SCREEN_SIZE/8, 64, 128);
-      }
-      if(map[player_lin-dir][player_col+dir] == 0) {
-         ctx.drawImage(img_Center2, 7*SCREEN_SIZE/16+64, 3*SCREEN_SIZE/8, 96, 128);
-      }
-
-      // Parede esquerda
-      if(map[player_lin-dir][player_col-dir] == 1) {
-         ctx.drawImage(img_Left2, SCREEN_SIZE/4, SCREEN_SIZE/4, 64, 256);
-      }
-      // Parede direita
-      if(map[player_lin-dir][player_col+dir] == 1) {
-         ctx.drawImage(img_Right2, 5*SCREEN_SIZE/8, SCREEN_SIZE/4, 64, 256);
-      }
-
-      return done;
-   }
-
-   function drawThirdLayer(player_lin, player_col, dir) {
-      // Checa terceira layer por paredes
-      if(map[player_lin-(3*dir)][player_col] == 1) {
-         ctx.drawImage(img_Center1, 7*SCREEN_SIZE/16, 7*SCREEN_SIZE/16, 64, 64);
-      }
-      if(map[player_lin-(2*dir)][player_col-dir] == 0) {
-         ctx.drawImage(img_Center1, 7*SCREEN_SIZE/16-32, 7*SCREEN_SIZE/16, 32, 64);
-      }
-      if(map[player_lin-(2*dir)][player_col+dir] == 0) {
-         ctx.drawImage(img_Center1, 7*SCREEN_SIZE/16+64, 7*SCREEN_SIZE/16, 32, 64);
-      }
-
-      // Parede esquerda
-      if(map[player_lin-(2*dir)][player_col-dir] == 1) {
-         ctx.drawImage(img_Left1, 3*SCREEN_SIZE/8, 3*SCREEN_SIZE/8, 32, 128);
-      }
-      // Parede direita
-      if(map[player_lin-(2*dir)][player_col+dir] == 1) {
-         ctx.drawImage(img_Right1, 9*SCREEN_SIZE/16, 3*SCREEN_SIZE/8, 32, 128);
-      }
+      ctx.putImageData(imageData, posX, posY);
    }
 
    function draw3DMap() {
-      let doneFront = false;
+      let done = false;
 
-      ctx.strokeStyle = "#3cff00"
+      for(let layer = 1; layer < 8 && !done; ++layer) {
+         // Pontos de guia
+         let offSet = SCREEN_SIZE/(2**layer);
+         let size = {
+            full:   offSet,
+            half:   offSet/2,
+            quart:  offSet/4,
+            double: 2*offSet,
+         }
+         let topLeft = {
+            x: SCREEN_SIZE/2-(offSet/2), 
+            y: SCREEN_SIZE/2-(offSet/2),
+         };
+         let topRight = {
+            x: SCREEN_SIZE/2+(offSet/2), 
+            y: SCREEN_SIZE/2-(offSet/2),
+         };
 
-      doneFront = drawFirstLayer (player_lin, player_col, dir);
-      if (!doneFront) {
-         doneFront = drawSecondLayer(player_lin, player_col, dir);
+         // Checa  layer por paredes frontais diretas
+         if(map[player_lin-(layer*dir)][player_col] == 1) {
+            ctx.drawImage(spr_wall_center, topLeft.x, topLeft.y, size.full, size.full);
+            darkenLayer(topLeft.x, topLeft.y, size.full, size.full, layer);
+            done = true;
+         }
+
+         // Paredes frontais indiretas (para os lados)
+         if(map[player_lin-(layer*dir-1)][player_col-dir] == 0) { //esquerda
+            ctx.drawImage(spr_wall_center, topLeft.x-size.half, topLeft.y, size.half, size.full);
+            darkenLayer(topLeft.x-size.half, topLeft.y, size.half, size.full, layer);
+         }
+         if(map[player_lin-(layer*dir-1)][player_col+dir] == 0) { //direita
+            ctx.drawImage(spr_wall_center, topLeft.x+size.full, topLeft.y, size.half, size.full);
+            darkenLayer(topLeft.x+size.full, topLeft.y, size.half, size.full, layer);
+         }
+
+         // Parede esquerda
+         if(map[player_lin-(layer*dir-1)][player_col-dir] == 1) {
+            ctx.drawImage(spr_wall_left, topLeft.x-size.half, topLeft.y-size.half, size.half, size.double);
+            darkenLayer(topLeft.x-size.half, topLeft.y-size.half, size.half, size.double, layer);
+         }
+         // Parede direita
+         if(map[player_lin-(layer*dir-1)][player_col+dir] == 1) {
+            ctx.drawImage(spr_wall_right, topRight.x, topRight.y-size.half, size.half, size.double);
+            darkenLayer(topRight.x, topRight.y-size.half, size.half, size.double, layer);
+         }
       }
-      if (!doneFront){
-         drawThirdLayer (player_lin, player_col, dir);
-      } 
    }
 
    function drawMiniMap() {
-      map.forEach((e, i) => {
-         let x = LINE_SIZE * (i % GRID_SIZE);
-         let y = LINE_SIZE * Math.floor(i / GRID_SIZE);
-         
-         // Se tile estiver ocupada, desenha branca
-         if(e) {
-            // Desenha minimapa
-            ctx.fillStyle = "white";
-            ctx.fillRect(x, y, LINE_SIZE, LINE_SIZE);
+      for(let i = 0; i < 8; ++i) {
+         for(let j = 0; j < 8; ++j) {
+            let x = SCREEN_SIZE-32+(4*j);
+            let y = SCREEN_SIZE-32+(4*i);
+            
+            // Se tile estiver ocupada, desenha branca
+            if(map[i][j]) {
+               // Desenha minimapa
+               ctx.fillStyle = "white";
+               ctx.fillRect(x, y, 4, 4);
+            }
+            else if(i == player_lin && j == player_col) {
+               ctx.fillStyle = "red";
+               ctx.fillRect(x, y, 4, 4);
+            }
          }
-         
-         // Desenha grid no mapa
-         ctx.strokeStyle = "black";
-         ctx.beginPath();
-
-         // Desenha linhas verticais
-         ctx.moveTo(x, y);
-         ctx.lineTo(x, y+LINE_SIZE);
-
-         // Desenha linhas horizontais
-         ctx.moveTo(x, y);
-         ctx.lineTo(x+LINE_SIZE, y);
-
-         ctx.stroke();
-      });
+      }
    }
 
    function gameLoop() {
@@ -203,8 +133,8 @@ if (canvas) {
       ctx.fillStyle = "black";
       ctx.fill();
 
-      //drawMiniMap();
       draw3DMap();
+      drawMiniMap();
    }
 
    document.addEventListener("keydown",
